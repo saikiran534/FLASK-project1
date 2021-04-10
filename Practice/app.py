b0a3d1fcb12c9366c00dd5735347a7fd9e7609d2
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-04-10 22:50:19 +0530

    HTML, Index,login,registration pages added

--- Practice/app.py
+++ Practice/app.py
@@ -3,3 +3,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("page1.html")
+    
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+        
+        page = user.query.filter_by(username=uname, password=passw).first()
+        if login is not None:
+            return redirect(url_for("page1"))
+    return render_template("login.html")
+

00bb734b5f046b3a1668b9cee10e7871810433c9
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-04-12 01:22:15 +0530

    added and displayed details

--- Practice/app.py
+++ Practice/app.py
@@ -1,10 +1,6 @@
-import os
+
 from flask import Flask, render_template, redirect, url_for, request
-from flask import Flask, session
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import Flask, render_template, request
+
 from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
@@ -19,28 +15,32 @@ app = Flask(__name__)
 def index():
     return render_template("page1.html")
 
-
-  
-
-
 @app.route("/register")
 def register():
   return render_template("register.html")
 
 @app.route("/submit", methods = ["GET","POST"])
 def submit():
-    if request.method == "GET":
-        return "Please submit the form"
-    else:
-        username = request.form.get("user name")
-        Email = request.form.get("Email")
-        Gender = request.form.get("gender")
-        Date_of_Birth = request.form.get("Date Of Birth")
-        Address = request.form.get("Address")
-        Password = request.form.get("Password")
+   
 
-        return render_template("submit.html", username=username,Email=Email,Date_of_Birth=Date_of_Birth,Address=Address,password=generate_password_hash(password, method='sha256'))
-        
+    # username = request.form.get("username")
+    # Email = request.form.get("Email")
+    # Gender = request.form.get("gender")
+    # Date_of_Birth = request.form.get("DateofBirth")
+    # Address = request.form.get("Address")
+    # Password = request.form.get("Password")
+    # print (username)
+    name = request.form.get("name")
+    password = request.form.get("password")
+    mobile = request.form.get("mobile")
+    dob = request.form.get("dob")
+    email = request.form.get("email")
+    gender = request.form.get("gender")
+    return render_template("submit.html", name=name, password=generate_password_hash(password, method='sha256'), mobile=mobile, dob=dob, email=email, gender=gender)
+
+
+    # return render_template("submit.html", username=username, Email=Email, Gender=Gender, Date_of_Birth=Date_of_Birth, Address=Address, Password=Password)
+        #generate_password_hash(Password, method='sha256)
 
 if __name__ == "__main__":
   app.run(debug=True)

a1ad9fbd1fabbed544b5ad5757a53a055e7386c6
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-04-12 10:21:47 +0530

    styling

--- Practice/app.py
+++ Practice/app.py
@@ -20,16 +20,7 @@ def register():
   return render_template("register.html")
 
 @app.route("/submit", methods = ["GET","POST"])
-def submit():
-   
-
-    # username = request.form.get("username")
-    # Email = request.form.get("Email")
-    # Gender = request.form.get("gender")
-    # Date_of_Birth = request.form.get("DateofBirth")
-    # Address = request.form.get("Address")
-    # Password = request.form.get("Password")
-    # print (username)
+def submit(): 
     name = request.form.get("name")
     password = request.form.get("password")
     mobile = request.form.get("mobile")
@@ -38,7 +29,13 @@ def submit():
     gender = request.form.get("gender")
     return render_template("submit.html", name=name, password=generate_password_hash(password, method='sha256'), mobile=mobile, dob=dob, email=email, gender=gender)
 
-
+    # username = request.form.get("username")
+    # Email = request.form.get("Email")
+    # Gender = request.form.get("gender")
+    # Date_of_Birth = request.form.get("DateofBirth")
+    # Address = request.form.get("Address")
+    # Password = request.form.get("Password")
+    # print (username)
     # return render_template("submit.html", username=username, Email=Email, Gender=Gender, Date_of_Birth=Date_of_Birth, Address=Address, Password=Password)
         #generate_password_hash(Password, method='sha256)
 
